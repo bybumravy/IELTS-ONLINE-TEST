0d321a8ed522cc500367470aef390cdd72702a6b
code-with-vinh <nqvinh036@gmail.com>
2025-05-30 09:29:01 +0700

    backend

---
+++ backend/ielts/src/test/java/web/ielts/IeltsApplicationTests.java
@@ -0,0 +1,13 @@
+package web.ielts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeltsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
