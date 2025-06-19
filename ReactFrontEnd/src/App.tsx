5cf9aa5dab36a3f73db4797a75d860835b4e799a
bybumravy <tnhavy97.9@gmail.com>
2025-06-19 22:52:14 +0700

    Da merge Viet

--- ReactFrontEnd/src/App.tsx
+++ ReactFrontEnd/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { HomePage } from "@/pages/HomePage"
-import IELTSWritingPractice from "@/pages/DoTest/WritingTest"
 import { AuthProvider } from "@/contexts/AuthContext"
 import Login from "@/components/sections/Login"
 import { MainLayout } from "@/components/layout/MainLayout"
@@ -15,16 +14,13 @@ import WritingTest from "@/pages/DoTest/WritingTest";
 import VoiceRecorder from "@/pages/DoTest/checkMic";
 import SpeakingTest from "@/pages/DoTest/SpeakingTest";
 import VerifyEmail from "@/components/sections/VerifyEmail";
-import Component from "@/pages/IELTSResult";
-
-import Component from "@/pages/DoTest/Component";
 import FulllTest from "@/pages/DoTest/FullTest";
 export default function App() {
     return (
         <AuthProvider>
             <Router>
                 <Routes>
-                    <Route path="/result" element={<MainLayout><Component /></MainLayout>}/>
+
                     <Route path="/" element={<MainLayout><HomePage /></MainLayout>}/>
                     <Route path="/login" element={<Login />}/>
                     <Route path="/student/listAllTips" element={<MainLayout><TipPage /></MainLayout>}/>
@@ -33,17 +29,15 @@ export default function App() {
                     <Route path="/test/reading/:testId" element={<ReadingTest />} />
                     <Route path="/test/writing/:testId" element={<WritingTest />} />
                     <Route path="/test/full/:testId" element={<FulllTest />} />
-                    {/*<Route path="/test/speaking/:testId" element={<SpeakingTest />} />*/}
-                    <Route path="/writing-result/:resultId" element={<MainLayout><WritingResult /></MainLayout>} />
-
-                    <Route path="/:skill/:id" element={<MainLayout><TipDetail /></MainLayout>} />
-                    <Route path="/tips/:skill" element={<MainLayout><TipPage/></MainLayout>} />
-                    <Route path="/test" element={<MainLayout><ListTestPage/></MainLayout>} />
-                    <Route path="/test/:skill" element={<MainLayout><ListTestPage /></MainLayout>} />
                     <Route path="/test/speaking/:testId" element={<VoiceRecorder />} />
                     <Route path="/checkMic/:testId" element={<SpeakingTest/>} />
+                    {/*<Route path="/test/speaking/:testId" element={<SpeakingTest />} />*/}
+                    <Route path="/writing-result/:resultId" element={<MainLayout><WritingResult /></MainLayout>} />
+                    <Route path="/:skill/:id" element={<MainLayout><TipDetail/></MainLayout>} />
+                    <Route path="/tips/:skill" element={<MainLayout><TipPage/></MainLayout>} />
+                    <Route path="/test" element={<MainLayout><ListTestPage/></MainLayout>} />
+                    <Route path="/test/:skill" element={<MainLayout><ListTestPage/></MainLayout>} />
                     <Route path="/verify-email" element={<VerifyEmail/>} />
-                    <Route path="/result" element={<MainLayout><Component/></MainLayout> as React.ReactNode} />
                 </Routes>
             </Router>
         </AuthProvider>
